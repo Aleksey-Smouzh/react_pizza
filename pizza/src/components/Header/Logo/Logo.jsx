cfb4557ad83f46b92532d43ff8d659be11d83cad
Aleksey-Smouzh <xmz@tut.by>
2022-03-13 21:24:21 +0300

    created Logo

--- pizza/src/components/Header/Logo/Logo.jsx
+++ pizza/src/components/Header/Logo/Logo.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import logo from './Logo.module.css'
+import imgLogo from '../../img/pizza-logo.svg'
+
 
 function Logo() {
   return (
-    <div>Logo</div>
+    <div className={logo.container}>
+       <h1 className={logo.title}>REACT PIZZA</h1>
+       <span className={logo.slogan}>самая вкусная пицца во вселенной</span>
+       <img className={logo.img} src={imgLogo} alt="a piece of pizza" />
+
+
+
+    </div>
   )
 }
 
