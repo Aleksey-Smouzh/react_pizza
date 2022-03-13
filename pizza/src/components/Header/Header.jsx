694beb847854b4746cd3e5a2218788ee4b7dd7b5
Aleksey-Smouzh <xmz@tut.by>
2022-03-13 22:00:44 +0300

    syle comp. Logo

---
+++ pizza/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import header from './Header.module.css'
+import Logo from './Logo/Logo'
+
+function Header() {
+  return (
+    <div className={header.container}>
+    
+    <Logo/>
+    
+    
+    
+    
+    
+    
+    </div>
+  )
+}
+
+export default Header
