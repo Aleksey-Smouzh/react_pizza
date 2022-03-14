642fcc79a1c9d2e331aef7abb5e349d3b52c3e77
Aleksey-Smouzh <xmz@tut.by>
2022-03-14 20:29:05 +0300

    add comp.MainPage

---
+++ pizza/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import main from "./MainPage.module.css"
+function MainPage() {
+  return (
+    <div className={main.container}>MainPage</div>
+  )
+}
+
+export default MainPage
