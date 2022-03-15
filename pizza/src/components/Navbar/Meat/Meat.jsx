ccca9ff4f66dcf81dcb83842c5bdd1cec9ad1899
Aleksey-Smouzh <xmz@tut.by>
2022-03-15 19:54:38 +0300

    add style comp Meat

--- pizza/src/components/Navbar/Meat/Meat.jsx
+++ pizza/src/components/Navbar/Meat/Meat.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-import meat from "./Meat.module.css"
+import React from "react";
+import meat from "./Meat.module.css";
+import SamplePizza from "../../SamplePizza/SamplePizza";
 function Meat() {
   return (
-    <div className={meat.container}>Meat</div>
-  )
+    <div className={meat.container}>
+      <h1 className={meat.title}>Мясная</h1>
+      <div className={meat.wrapper}>
+        <div className={meat.blok1}>
+          <SamplePizza />
+        </div>
+        <div className={meat.blok2}>
+          <SamplePizza />
+        </div>
+        <div className={meat.blok3}>
+          <SamplePizza />
+        </div>
+        <div className={meat.blok4}>
+          <SamplePizza />
+        </div>
+        <div className={meat.blok5}>
+          <SamplePizza />
+        </div>
+        <div className={meat.blok6}>
+          <SamplePizza />
+        </div>
+        <div className={meat.blok7}>
+          <SamplePizza />
+        </div>
+        <div className={meat.blok8}>
+          <SamplePizza />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Meat
+export default Meat;
