60cb7b47995bb8aac6b6c196a58b487bfa7e3b2b
Aleksey-Smouzh <xmz@tut.by>
2022-03-15 19:55:08 +0300

    add style comp.Grill

--- pizza/src/components/Navbar/Grill/Grill.jsx
+++ pizza/src/components/Navbar/Grill/Grill.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-import grill from "./Grill.module.css"
+import React from "react";
+import grill from "./Grill.module.css";
+import SamplePizza from "../../SamplePizza/SamplePizza";
 function Grill() {
   return (
-    <div className={grill.container}>Grill</div>
-  )
+    <div className={grill.container}>
+      <h1 className={grill.title}>Гриль</h1>
+      <div className={grill.wrapper}>
+        <div className={grill.blok1}>
+          <SamplePizza />
+        </div>
+        <div className={grill.blok2}>
+          <SamplePizza />
+        </div>
+        <div className={grill.blok3}>
+          <SamplePizza />
+        </div>
+        <div className={grill.blok4}>
+          <SamplePizza />
+        </div>
+        <div className={grill.blok5}>
+          <SamplePizza />
+        </div>
+        <div className={grill.blok6}>
+          <SamplePizza />
+        </div>
+        <div className={grill.blok7}>
+          <SamplePizza />
+        </div>
+        <div className={grill.blok8}>
+          <SamplePizza />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Grill
+export default Grill;
