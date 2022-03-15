0fdf40f663f5c7131fd12866a2a586d406015540
Aleksey-Smouzh <xmz@tut.by>
2022-03-15 19:54:02 +0300

    add ctyle to comp. Spicy, Vegan

--- pizza/src/components/Navbar/Spicy/Spicy.jsx
+++ pizza/src/components/Navbar/Spicy/Spicy.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-import spicy from './Spicy.module.css'
+import React from "react";
+import spicy from "./Spicy.module.css";
+import SamplePizza from "../../SamplePizza/SamplePizza";
 function Spicy() {
   return (
-    <div className={spicy.container}>Spicy</div>
-  )
+    <div className={spicy.container}>
+      <h1 className={spicy.title}>Острая</h1>
+      <div className={spicy.wrapper}>
+        <div className={spicy.blok1}>
+          <SamplePizza />
+        </div>
+        <div className={spicy.blok2}>
+          <SamplePizza />
+        </div>
+        <div className={spicy.blok3}>
+          <SamplePizza />
+        </div>
+        <div className={spicy.blok4}>
+          <SamplePizza />
+        </div>
+        <div className={spicy.blok5}>
+          <SamplePizza />
+        </div>
+        <div className={spicy.blok6}>
+          <SamplePizza />
+        </div>
+        <div className={spicy.blok7}>
+          <SamplePizza />
+        </div>
+        <div className={spicy.blok8}>
+          <SamplePizza />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Spicy
+export default Spicy;
--- pizza/src/components/Navbar/Vegan/Vegan.jsx
+++ pizza/src/components/Navbar/Vegan/Vegan.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-import vegan from './Vegan.module.css'
+import React from "react";
+import vegan from "./Vegan.module.css";
+import SamplePizza from "../../SamplePizza/SamplePizza";
 function Vegan() {
   return (
-    <div className={vegan.container}>Vegan</div>
-  )
+    <div className={vegan.container}>
+      <h1 className={vegan.title}>Вегетарианская</h1>
+      <div className={vegan.wrapper}>
+        <div className={vegan.blok1}>
+          <SamplePizza />
+        </div>
+        <div className={vegan.blok2}>
+          <SamplePizza />
+        </div>
+        <div className={vegan.blok3}>
+          <SamplePizza />
+        </div>
+        <div className={vegan.blok4}>
+          <SamplePizza />
+        </div>
+        <div className={vegan.blok5}>
+          <SamplePizza />
+        </div>
+        <div className={vegan.blok6}>
+          <SamplePizza />
+        </div>
+        <div className={vegan.blok7}>
+          <SamplePizza />
+        </div>
+        <div className={vegan.blok8}>
+          <SamplePizza />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Vegan
+export default Vegan;
